aad9c6ac1bd34f7b99153be73453767db0efb68a
Aniekan Ido <65046548+aniekanido@users.noreply.github.com>
2022-02-25 23:52:30 +0100

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,8 @@
-# Display the output
-print('New Python File')
+# display numbers counting down from 10 to 1
+myCounter = 10
+
+while myCounter >= 1:
+    print(myCounter)
+    myCounter -=1
+    
+print('i have escaped the loop')
